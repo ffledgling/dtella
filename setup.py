3e98ca8a978aabfdb9d035143b6de9434d25a267
feisley <jacob.feisley@gmail.com>
2007-01-16 05:05:56 +0000

    Fixed VIM tab problem

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 import py2exe
 setup(
-	options={"py2exe":{"optimize":2}},
-	zipfile=None,
+    options={"py2exe":{"optimize":2}},
+    zipfile=None,
 
-	#windows=[{
-	#    "script": "dtella.py",
-	#    "icon_resources": [(1, "dcgate.ico")]
-	#}]
+    #windows=[{
+    #    "script": "dtella.py",
+    #    "icon_resources": [(1, "dcgate.ico")]
+    #}]
 
-    	console=[{"script": "dtella.py"}]
+    console=[{"script": "dtella.py"}]
 )
 
