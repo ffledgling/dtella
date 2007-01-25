0a132eff55845a8518ba04555baba81d5fec60d6
paul248 <paul@dtella.org>
2007-01-25 08:05:56 +0000

    Updated py2exe batch file

--- build_py2exe.bat
+++ build_py2exe.bat
@@ -1,1 +1,7 @@
-c:\python25\python setup.py py2exe -O2 --ascii --excludes _ssl --bundle 1
+@ECHO OFF
+REM Exclude SSL and XML because they're big and not used
+REM --ascii prevents a million other encodings from getting added
+REM --bundle 1 bundles the DLLs into the EXE.
+
+c:\python25\python setup.py py2exe -O2 --ascii --exclude xml,_ssl --bundle 1
+pause
