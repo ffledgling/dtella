5c38898fd38430bd4e0d6bfa16888d709fa7ba13
paul248 <paul@dtella.org>
2007-02-06 06:38:33 +0000

    Update py2exe batch file.

--- build_py2exe.bat
+++ build_py2exe.bat
@@ -3,6 +3,12 @@ REM Exclude SSL and XML because they're big and not used
 REM --ascii prevents a million other encodings from getting added
 REM --bundle 1 bundles the DLLs into the EXE.
 
+dist\dtella.exe --terminate
+
+REM delete old directores
+rmdir /s /q build
+rmdir /s /q dist
+
 c:\python25\python setup.py py2exe -O2 --ascii --exclude xml,_ssl --bundle 1
 
 REM Dtella doesn't use os.popen, so...
