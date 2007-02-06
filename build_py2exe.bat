dd6b08420298bb620c550f76714f620ceac73c96
paul248 <paul@dtella.org>
2007-02-06 22:35:25 +0000

    Added script to build the source package and installer.

--- build_py2exe.bat
+++ build_py2exe.bat
@@ -1,4 +1,3 @@
-@ECHO OFF
 REM Exclude SSL and XML because they're big and not used
 REM --ascii prevents a million other encodings from getting added
 REM --bundle 1 bundles the DLLs into the EXE.
@@ -11,7 +10,7 @@ rmdir /s /q dist
 
 c:\python25\python setup.py py2exe -O2 --ascii --exclude xml,_ssl --bundle 1
 
-REM Dtella doesn't use os.popen, so...
-del dist\w9xpopen.exe
+copy dist\dtella.exe installer
+copy dist\msvcr71.dll installer
 
 pause
---
+++ installer/build_installer.bat
@@ -0,0 +1,48 @@
+set DTDIR="dtella-purdue-0.9"
+set ARC="C:\Program Files\7-Zip\7z.exe"
+set NSIS="C:\Program Files\NSIS\makensisw.exe"
+
+
+REM ------- SOURCE CODE ---------
+
+rmdir /s /q %DTDIR%
+del %DTDIR%.tar
+del %DTDIR%.tar.bz2
+
+mkdir %DTDIR%
+
+copy ..\dtella.py                %DTDIR%
+copy ..\dtella_bridgeclient.py   %DTDIR%
+copy ..\dtella_core.py           %DTDIR%
+copy ..\dtella_crypto.py         %DTDIR%
+copy ..\dtella_dc.py             %DTDIR%
+copy ..\dtella_dnslookup.py      %DTDIR%
+copy ..\dtella_fixtwistedtime.py %DTDIR%
+copy ..\dtella_local.py          %DTDIR%
+copy ..\dtella_state.py          %DTDIR%
+copy ..\docs\readme.txt          %DTDIR%
+copy ..\docs\gpl.txt             %DTDIR%
+
+%ARC% a -ttar %DTDIR%.tar %DTDIR%
+%ARC% a -tbzip2 %DTDIR%.tar.bz2 %DTDIR%.tar
+
+del %DTDIR%.tar
+rmdir /s /q %DTDIR%
+
+
+REM ------- EXE -------------
+
+pushd ..
+call build_py2exe
+popd
+
+REM ------- DOCS ------------
+
+copy ..\docs\readme.txt .
+
+
+REM ------- INSTALLER -------
+
+%NSIS% dtella.nsi
+
+pause
