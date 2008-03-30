87a25c0cdecf4667329757d6905e0a1c95a80e43
paul248 <paul@dtella.org>
2008-03-30 05:53:03 +0000

    - The DNS bugs were fixed in Twisted 8, so remove the ugly patching. - On Windows, wrap time.clock in NonDecreasingTimer, because of worries   that it might wrap after 49 days. - Change some formatting for no particular reason.

--- dtella/common/fix_twisted.py
+++ dtella/common/fix_twisted.py
@@ -24,81 +24,42 @@ import time
 import os
 import twisted.python.runtime
 
-# Twisted 2.4.0 uses the system time for firing events.  This could lead to
+# Twisted uses the system time for firing events.  This could lead to
 # really bad things if the user fiddles with their clock.
 
 # This code should help until they actually fix Twisted.
 
-if os.name == 'nt':
-    # time.clock works nicely in Windows
-    twisted.python.runtime.seconds = time.clock
-else:
-    class NonDecreasingTimer:
-        # This class lets us count the time between events, whilst mostly
-        # hiding the effect of large changes in the system clock.    
-        def __init__(self):
-            self.last = time.time()
-            self.counter = 0.0
+class NonDecreasingTimer:
+    # This class lets us count the time between events, whilst mostly
+    # hiding the effect of large changes in the system clock.    
+    def __init__(self):
+        self.last = time.time()
+        self.counter = 0.0
 
-        def seconds(self):
-            # This method should be called at least once per minute to
-            # keep the timer going.  If the system time moves backwards,
-            # the timer will stay put.  If the time moves forwards,
-            # the timer will increment, but never by more than 1 minute.
-            tt = time.time()
-            self.counter += min(60.0, max(0.0, tt-self.last))
-            self.last = tt
-            return self.counter
+        # On Windows, time.clock is more stable, but rumor on the street
+        # is that it loops after 49 days.
+        if os.name == 'nt':
+            self.timesrc = time.clock
+        else:
+            self.timesrc = time.time
+
+    def seconds(self):
+        # This method should be called at least once per minute to
+        # keep the timer going.  If the system time moves backwards,
+        # the timer will stay put.  If the time moves forwards,
+        # the timer will increment, but never by more than 1 minute.
+        now = self.timesrc()
+        self.counter += min(60.0, max(0.0, now-self.last))
+        self.last = now
+        return self.counter
     
-    twisted.python.runtime.seconds = NonDecreasingTimer().seconds
+twisted.python.runtime.seconds = NonDecreasingTimer().seconds
 
-    # Ensure that seconds() gets called at least once a minute
-    from twisted.internet import reactor
+# Ensure that seconds() gets called at least once a minute
+from twisted.internet import reactor
 
-    def noop():
-        reactor.callLater(55.0, noop)
+def noop():
+    reactor.callLater(55.0, noop)
 
-    noop()
-
-
-def fixTwistedNames():
-    import twisted.names.dns
-    import socket
-
-    # bugfix: splice some error trapping into the writeMessage function
-    # http://twistedmatrix.com/trac/ticket/2492
-
-    f1 = twisted.names.dns.DNSDatagramProtocol.writeMessage
-    def writeMessage(*args):
-        try:
-            f1(*args)
-        except socket.error:
-            pass
-    twisted.names.dns.DNSDatagramProtocol.writeMessage = writeMessage
-
-    # Another workaround:
-    # http://twistedmatrix.com/trac/ticket/2581
-
-    from twisted.internet.error import CannotListenError
-
-    f2 = twisted.names.dns.DNSDatagramProtocol.startListening
-    def startListening(*args):
-        for i in range(10):
-            try:
-                return f2(*args)
-            except CannotListenError:
-                pass
-        raise AssertionError("DNS isn't happy :-(")
-    twisted.names.dns.DNSDatagramProtocol.startListening = startListening
-
-
-# Don't care right now if twisted names can't be imported, because it might
-# not be required.
-try:
-    import twisted.names
-except ImportError:
-    pass
-else:
-    if twisted.names.__version__ == '0.4.0':
-        fixTwistedNames()
+noop()
 
