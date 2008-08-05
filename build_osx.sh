fe35348dd69a55156d6157c05416b83917beec1e
paul248 <paul@dtella.org>
2008-08-05 06:40:12 +0000

    Tweak OSX build script.

--- build_osx.sh
+++ build_osx.sh
@@ -13,5 +13,5 @@ cp docs/readme.txt dist/
 cp docs/changelog.txt dist/
 cp docs/gpl.txt dist/
 
-python setup.py py2app
+python setup.py py2app || exit
 hdiutil create -srcfolder dist -volname $FILE_BASE $FILE_BASE.dmg
