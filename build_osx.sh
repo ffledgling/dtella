700bd116767306e1aae12f9699a99637306884b0
feisley <jacob.feisley@gmail.com>
2008-08-07 05:31:55 +0000

    - Updated OS X build script to use new install template

--- build_osx.sh
+++ build_osx.sh
@@ -5,7 +5,9 @@ else
   FILE_BASE="dtella-$1"
 fi
 
+rm -r build
 rm -r dist
+rm installer_osx/template.sparseimage
 rm $FILE_BASE.dmg
 
 mkdir dist
@@ -14,4 +16,18 @@ cp docs/changelog.txt dist/
 cp docs/gpl.txt dist/
 
 python setup.py py2app || exit
-hdiutil create -srcfolder dist -volname $FILE_BASE $FILE_BASE.dmg
+
+hdiutil convert installer_osx/template.dmg -format UDSP -o installer_osx/template
+hdiutil mount installer_osx/template.sparseimage
+
+cp -R dist/dtella.app/ /Volumes/Dtella/Dtella.app
+cp dist/readme.txt /Volumes/Dtella/
+cp dist/changelog.txt /Volumes/Dtella/
+cp dist/gpl.txt /Volumes/Dtella/
+
+diskutil rename /Volumes/Dtella/ $FILE_BASE
+hdiutil eject /Volumes/$FILE_BASE
+
+hdiutil convert installer_osx/template.sparseimage -format UDBZ -o $FILE_BASE.dmg
+
+rm installer_osx/template.sparseimage
