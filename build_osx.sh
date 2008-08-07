09d9ff576e44dc854001badc5751ac83b0d016f4
feisley <jacob.feisley@gmail.com>
2008-08-07 06:02:08 +0000

    - Minor corrections to the OS X build script

--- build_osx.sh
+++ build_osx.sh
@@ -10,20 +10,19 @@ rm -r dist
 rm installer_osx/template.sparseimage
 rm $FILE_BASE.dmg
 
-mkdir dist
-cp docs/readme.txt dist/
-cp docs/changelog.txt dist/
-cp docs/gpl.txt dist/
-
 python setup.py py2app || exit
 
+
+hdiutil eject /Volumes/Dtella
+hdiutil eject /Volumes/$FILE_BASE
+
 hdiutil convert installer_osx/template.dmg -format UDSP -o installer_osx/template
-hdiutil mount installer_osx/template.sparseimage
+hdiutil attach installer_osx/template.sparseimage
 
 cp -R dist/dtella.app/ /Volumes/Dtella/Dtella.app
-cp dist/readme.txt /Volumes/Dtella/
-cp dist/changelog.txt /Volumes/Dtella/
-cp dist/gpl.txt /Volumes/Dtella/
+cp docs/readme.txt /Volumes/Dtella/
+cp docs/changelog.txt /Volumes/Dtella/
+cp docs/gpl.txt /Volumes/Dtella/
 
 diskutil rename /Volumes/Dtella/ $FILE_BASE
 hdiutil eject /Volumes/$FILE_BASE
