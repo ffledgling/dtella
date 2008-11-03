aa54aac8bdf238a27fd7da4809c8d5815eec6c68
feisley <jacob.feisley@gmail.com>
2008-11-03 15:00:49 +0000

    - Updated OSX build script to match the windows script (both output now to dist)

--- build_osx.sh
+++ build_osx.sh
@@ -5,18 +5,20 @@ else
   FILE_BASE="dtella-$1"
 fi
 
+BLDIR="installer_osx"
+OUTDIR="dist"
+
 rm -r build
-rm -r dist
-rm installer_osx/template.sparseimage
-rm $FILE_BASE.dmg
+rm $BLDIR/template.sparseimage
+rm $OUTDIR/$FILE_BASE.dmg
 
 python setup.py py2app || exit
 
 hdiutil eject /Volumes/Dtella
 hdiutil eject /Volumes/$FILE_BASE
 
-hdiutil convert installer_osx/template.dmg -format UDSP -o installer_osx/template
-hdiutil attach installer_osx/template.sparseimage
+hdiutil convert $BLDIR/template.dmg -format UDSP -o $BLDIR/template
+hdiutil attach $BLDIR/template.sparseimage
 
 cp -R dist/dtella.app/ /Volumes/Dtella/Dtella.app
 cp docs/readme.txt /Volumes/Dtella/
@@ -26,6 +28,8 @@ cp docs/gpl.txt /Volumes/Dtella/
 diskutil rename /Volumes/Dtella/ $FILE_BASE
 hdiutil eject /Volumes/$FILE_BASE
 
-hdiutil convert installer_osx/template.sparseimage -format UDBZ -o $FILE_BASE.dmg
+hdiutil convert $BLDIR/template.sparseimage -format UDBZ -o $FILE_BASE.dmg
 
-rm installer_osx/template.sparseimage
+cp $FILE_BASE.dmg $OUTDIR
+
+rm $BLDIR/template.sparseimage
