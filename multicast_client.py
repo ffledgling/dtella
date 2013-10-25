9770be404d790230bd0a8a3b6aca313e5b7342c8
gnu_user <gsoc.student@gmail.com>
2013-10-24 21:55:13 -0400

    Added the example multicast client and server

---
+++ multicast_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+
+class MulticastPingClient(DatagramProtocol):
+
+    def startProtocol(self):
+        # Join the multicast address, so we can receive replies:
+        self.transport.setTTL(63)
+        self.transport.joinGroup("239.0.0.2")
+        # Send to 228.0.0.5:8005 - all listeners on the multicast address
+        # (including us) will receive this message.
+        self.transport.write('Client: Ping', ("239.0.0.2", 8005))
+
+    def datagramReceived(self, datagram, address):
+        print "Datagram %s received from %s" % (repr(datagram), repr(address))
+
+
+reactor.listenMulticast(8005, MulticastPingClient(), listenMultiple=True)
+reactor.run()
---
+++ multicast_server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+
+class MulticastPingPong(DatagramProtocol):
+
+    def startProtocol(self):
+        """
+        Called after protocol has started listening.
+        """
+        # Set the TTL>1 so multicast will cross router hops:
+        self.transport.setTTL(63)
+        # Join a specific multicast group:
+        self.transport.joinGroup("239.0.0.2")
+
+    def datagramReceived(self, datagram, address):
+        print "Datagram %s received from %s" % (repr(datagram), repr(address))
+        if datagram == "Client: Ping":
+            # Rather than replying to the group multicast address, we send the
+            # reply directly (unicast) to the originating port:
+            self.transport.write("Server: Pong", address)
+
+
+# We use listenMultiple=True so that we can run MulticastServer.py and
+# MulticastClient.py on same machine:
+reactor.listenMulticast(8005, MulticastPingPong(),
+                        listenMultiple=True)
+reactor.run()
